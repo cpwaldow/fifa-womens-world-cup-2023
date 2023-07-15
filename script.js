7a02649644058bfbb38ff0c7918f288df939413b
Carlos Eduardo <cpwaldow@gmail.com>
2023-07-14 22:06:28 -0300

    criando a função para criar o elemento dos países

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ const handleDinamicText = (element, text) => {
   createEl.innerText = text;
   return createEl;
 };
+
+const handleCreateElement = (country) => {
+  const createDiv = document.createElement('div');
+  const createImg = document.createElement('img');
+  createImg.src = country.flag;
+  createDiv.appendChild(createImg);
+  createDiv.appendChild(handleDinamicText('p', country.name));
+  createDiv.appendChild(handleDinamicText('p', country.region));
+  createDiv.appendChild(
+    handleDinamicText(
+      'p',
+      `Population: ${formatCompactNumber(country.population)}`,
+    ),
+  );
+  return createDiv;
+};
