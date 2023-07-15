b7e1a9359d68219387a95f5f19e17426257df46f
Carlos Eduardo <cpwaldow@gmail.com>
2023-07-14 22:04:48 -0300

    criando função para criar elemento de texto dinamico

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function formatCompactNumber(number) {
   const formatter = Intl.NumberFormat('en', { notation: 'compact' });
   return formatter.format(number);
 }
+
+const handleDinamicText = (element, text) => {
+  const createEl = document.createElement(element);
+  createEl.innerText = text;
+  return createEl;
+};
