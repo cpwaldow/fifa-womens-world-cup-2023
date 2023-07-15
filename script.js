f45e5543a94be3d3a5e33ccc182b4687dd5da43b
Carlos Eduardo <cpwaldow@gmail.com>
2023-07-14 22:05:34 -0300

    alterando a função formatCompactNumber para arrowfunction

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 import { teams } from './data.js';
 
-function formatCompactNumber(number) {
+const formatCompactNumber = (number) => {
   const formatter = Intl.NumberFormat('en', { notation: 'compact' });
   return formatter.format(number);
-}
+};
 
 const handleDinamicText = (element, text) => {
   const createEl = document.createElement(element);
