c8f1d7bc017eef02165a40a4e7b391d24ff838ee
Carlos Eduardo <cpwaldow@gmail.com>
2023-07-14 22:03:53 -0300

    criando função para formatar os números de população

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 import { teams } from './data.js';
+
+function formatCompactNumber(number) {
+  const formatter = Intl.NumberFormat('en', { notation: 'compact' });
+  return formatter.format(number);
+}
